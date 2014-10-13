56e1e7b846e26f75680dbc37c8c4a03d40ba0b79
Evgeny Mandrikov <mandrikov@gmail.com>
2014-10-13 14:59:38 +0200

    Add more unit tests

--- src/test/java/com/sonarsource/lits/DumpPhaseTest.java
+++ src/test/java/com/sonarsource/lits/DumpPhaseTest.java
@@ -6,16 +6,23 @@
 package com.sonarsource.lits;
 
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import org.junit.Before;
 import org.junit.Test;
 import org.sonar.api.batch.DecoratorContext;
 import org.sonar.api.component.ResourcePerspectives;
+import org.sonar.api.issue.Issuable;
+import org.sonar.api.issue.Issue;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.resources.Resource;
 import org.sonar.api.resources.Scopes;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.api.rules.ActiveRule;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -50,6 +57,37 @@ public class DumpPhaseTest {
     decorator.decorate(resource, context);
 
     verify(checker).save();
+  }
+
+  @Test
+  public void should_report_missing_issues() {
+    Resource resource = mockProject();
+    DecoratorContext context = mock(DecoratorContext.class);
+
+    Multiset<IssueKey> issues = HashMultiset.create();
+    issues.add(new IssueKey("", "squid:S00103", null));
+    when(checker.getByComponentKey(anyString())).thenReturn(issues);
+
+    ActiveRule activeRule = mock(ActiveRule.class);
+    when(rulesProfile.getActiveRule("squid", "S00103")).thenReturn(activeRule);
+
+    Issuable.IssueBuilder issueBuilder = mockIssueBuilder();
+    Issuable issuable = mock(Issuable.class);
+    when(issuable.newIssueBuilder()).thenReturn(issueBuilder);
+    when(resourcePerspectives.as(eq(Issuable.class), any(Resource.class))).thenReturn(issuable);
+
+    decorator.decorate(resource, context);
+
+    verify(issuable).addIssue(any(Issue.class));
+  }
+
+  private Issuable.IssueBuilder mockIssueBuilder() {
+    Issuable.IssueBuilder issueBuilder = mock(Issuable.IssueBuilder.class);
+    when(issueBuilder.ruleKey(any(RuleKey.class))).thenReturn(issueBuilder);
+    when(issueBuilder.severity(any(String.class))).thenReturn(issueBuilder);
+    when(issueBuilder.line(any(Integer.class))).thenReturn(issueBuilder);
+    when(issueBuilder.message(any(String.class))).thenReturn(issueBuilder);
+    return issueBuilder;
   }
 
   private Resource mockProject() {
