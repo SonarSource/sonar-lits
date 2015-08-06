97d17d8b98f11ea481d66b2561879298010a4b0e
Massimo Paladin <massimo.paladin@gmail.com>
2015-08-06 11:45:13 +0200

    Add missing package-info file

---
+++ src/test/java/com/sonarsource/lits/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2013-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.sonarsource.lits;
