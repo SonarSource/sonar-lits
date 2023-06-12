3e48de6ba5241c3a2780a0fe54b3a1190e913026
Evgeny Mandrikov <mandrikov@gmail.com>
2023-06-12 12:11:32 +0200

    (WIP) remove unused imports

--- sonar-lits-plugin/src/main/java/com/sonarsource/lits/Dump.java
+++ sonar-lits-plugin/src/main/java/com/sonarsource/lits/Dump.java
@@ -31,13 +31,11 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
