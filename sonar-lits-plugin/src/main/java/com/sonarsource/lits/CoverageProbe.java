a462272ba97ae22f7d859de9f98e702eaa881a5b
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2026-09-24 17:18:51 +0200

    CLP-897 Add temporary covered and uncovered code probe

---
+++ sonar-lits-plugin/src/main/java/com/sonarsource/lits/CoverageProbe.java
@@ -0,0 +1,27 @@
+/*
+ * Sonar LITS Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package com.sonarsource.lits;
+
+final class CoverageProbe {
+
+  static int select(boolean covered) {
+    if (covered) {
+      return 1;
+    }
+    return 2;
+  }
+}
